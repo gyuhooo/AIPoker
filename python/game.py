a8994f91358813e0bcd73b30ecfb8ba1c4ed2cb1
flaireclair <h.kmntag6.gaburiasu@gmail.com>
2018-12-05 00:23:48 +0900

    fixed error

--- python/game.py
+++ python/game.py
@@ -9,6 +9,7 @@ SCREEN_SIZE = (640, 480)
 GREEN = (88, 191, 63)
 root = "../images/cards/"
 cards = []
+event = []
 flag = [False, False, False, False, False]
 
 class Porker(object) :
@@ -19,9 +20,12 @@ class Porker(object) :
     font = pygame.font.Font("../images/Pacifico.ttf", 40)
     clock = pygame.time.Clock()
     
-    for no in db.num :
+    for no in db.player_num :
         cards.append(pygame.image.load("%s%d.png" % (root, no)).convert_alpha())
-         
+    
+    for eve in pygame.event.get() :
+        event.append(eve)
+
     while(1) :
         clock.tick(60)
         screen.fill(GREEN)
@@ -30,17 +34,19 @@ class Porker(object) :
         #print(x, y)
 
         j = 0
-        while (j < 5) :                
+        while (j < 5) :
             if flag[j] :                
                 if 260 <= y <= 410 :
                     if j == 4 :
                         if 360 < x <= 430 :
                             if event.type == MOUSEBUTTONDOWN and event.button == 1 :
                                 flag[j] = False
+                            screen.blit(cards[j], (200 + j * 40, 260))
                     else :
                         if 200 + j * 40 < x <= 240 + j * 40 :
                             if event.type == MOUSEBUTTONDOWN and event.button == 1 :
                                 flag[j] = False
+                            screen.blit(cards[j], (200 + j * 40, 260))
                 screen.blit(cards[j], (200 + j * 40, 260))
             else :
                 if 300 <= y <= 410 :
@@ -61,7 +67,7 @@ class Porker(object) :
                 else :
                     screen.blit(cards[j], (200 + j * 40, 300))
                         
-        j += 1
+            j += 1
 
         screen.blit(font.render(player.comment, True, (255, 0, 0)), (160, 220))
     
@@ -77,14 +83,14 @@ class Porker(object) :
 
         pygame.display.update()
 
-
+"""
 class Card_choice(pygame.sprite.Sprite) :
     animecycle = 12
     frame = 0
     def __init__(self, card, x, y) :
         pygame.sprite.Sprite.__init__(self, self.containers)
         
-
+"""
              
 if __name__ == "__main__" :
     MY_GAME = Porker()
