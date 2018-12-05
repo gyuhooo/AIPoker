0f4ad45f71c9fae5f35da38e06f49fc2f6211f4d
flaireclair <h.kmntag6.gaburiasu@gmail.com>
2018-12-05 09:40:43 +0900

    fixed animation

--- python/game.py
+++ python/game.py
@@ -10,79 +10,107 @@ GREEN = (88, 191, 63)
 root = "../images/cards/"
 cards = []
 event = []
-flag = [False, False, False, False, False]
+flag = []
+GAME_MODE = {'START' : 0, 'PLAY' : 1}
+game_state, x, y, screen, font = 0, 0, 0, 0, 0
 
 class Porker(object) :
-    pygame.init()
-    pygame.font.init
-    screen = pygame.display.set_mode(SCREEN_SIZE)
-    pygame.display.set_caption('Porker')
-    font = pygame.font.Font("../images/Pacifico.ttf", 40)
-    clock = pygame.time.Clock()
     
-    for no in db.player_num :
-        cards.append(pygame.image.load("%s%d.png" % (root, no)).convert_alpha())
+    def __init__(self) :
+        pygame.init()
+        pygame.font.init
+        self.event = pygame.event.get()
+        self.screen = pygame.display.set_mode(SCREEN_SIZE)
+        pygame.display.set_caption('Porker')
+        self.font = pygame.font.Font("../images/Pacifico.ttf", 40)
+        self.clock = pygame.time.Clock()
+        self.game_state = GAME_MODE['START']
+        self.flag = [False, False, False, False, False]
     
-    for eve in pygame.event.get() :
-        event.append(eve)
+        for no in db.player_num :
+            cards.append(pygame.image.load("%s%d.png" % (root, no)).convert_alpha())
+            
+        
+        while(1) :
+            self.clock.tick(60)
+            self.screen.fill(GREEN)
+            self.x, self.y = pygame.mouse.get_pos()
+            self.event = pygame.event.get()
+            self.update()
+            pygame.display.update()
+            self.key_handler()
+            pygame.event.clear()
+            
 
-    while(1) :
-        clock.tick(60)
-        screen.fill(GREEN)
-        x, y = pygame.mouse.get_pos()
-        #for debug
-        #print(x, y)
+    def update(self) :
+        if self.game_state == GAME_MODE['PLAY'] :
+            self.play()
+        elif self.game_state == GAME_MODE['START'] :
+            self.start()
 
+    def play(self) :
         j = 0
         while (j < 5) :
-            if flag[j] :                
-                if 260 <= y <= 410 :
+            if self.flag[j] :                
+                if 260 <= self.y <= 410 :
                     if j == 4 :
-                        if 360 < x <= 430 :
-                            if event.type == MOUSEBUTTONDOWN and event.button == 1 :
-                                flag[j] = False
-                            screen.blit(cards[j], (200 + j * 40, 260))
+                        if 360 < self.x <= 430 :                            
+                            for eve in self.event :
+                                if eve.type == MOUSEBUTTONDOWN and eve.button == 1 :
+                                    self.flag[j] = False
+                            self.screen.blit(cards[j], (200 + j * 40, 260))
                     else :
-                        if 200 + j * 40 < x <= 240 + j * 40 :
-                            if event.type == MOUSEBUTTONDOWN and event.button == 1 :
-                                flag[j] = False
-                            screen.blit(cards[j], (200 + j * 40, 260))
-                screen.blit(cards[j], (200 + j * 40, 260))
+                        if 200 + j * 40 < self.x <= 240 + j * 40 :
+                            for eve in self.event :
+                                if eve.type == MOUSEBUTTONDOWN and eve.button == 1 :
+                                    self.flag[j] = False
+                            self.screen.blit(cards[j], (200 + j * 40, 260))
+                self.screen.blit(cards[j], (200 + j * 40, 260))
             else :
-                if 300 <= y <= 410 :
+                if 300 <= self.y <= 410 :
                     if j == 4 :
-                        if 360 < x <= 430 :
-                            if event.type == MOUSEBUTTONDOWN and event.button == 1 :
-                                flag[j] = True
-                            screen.blit(cards[j], (200 + j * 40, 260))
+                        if 360 < self.x <= 430 :
+                            for eve in self.event :
+                                if eve.type == MOUSEBUTTONDOWN and eve.button == 1 :
+                                    self.flag[j] = True
+                            self.screen.blit(cards[j], (200 + j * 40, 260))
                         else :
-                            screen.blit(cards[j], (200 + j * 40, 300))
+                            self.screen.blit(cards[j], (200 + j * 40, 300))
                     else :
-                        if 200 + j * 40 < x <= 240 + j * 40 :
-                            if event.type == MOUSEBUTTONDOWN and event.button == 1 :
-                                flag[j] = True
-                            screen.blit(cards[j], (200 + j * 40, 260))
+                        if 200 + j * 40 < self.x <= 240 + j * 40 :
+                            for eve in self.event :
+                                if eve.type == MOUSEBUTTONDOWN and eve.button == 1 :
+                                    self.flag[j] = True
+                            self.screen.blit(cards[j], (200 + j * 40, 260))
                         else :
-                            screen.blit(cards[j], (200 + j * 40, 300))
+                            self.screen.blit(cards[j], (200 + j * 40, 300))
                 else :
-                    screen.blit(cards[j], (200 + j * 40, 300))
+                    self.screen.blit(cards[j], (200 + j * 40, 300))
                         
             j += 1
 
-        screen.blit(font.render(player.comment, True, (255, 0, 0)), (160, 220))
+        self.screen.blit(self.font.render(player.comment, True, (255, 0, 0)), (160, 220))
     
-        for event in pygame.event.get() :
-                if event.type == QUIT :
-                    pygame.quit()
+    def key_handler(self) :
+        if self.event != [] :
+            for eve in self.event :
+                if eve.type == QUIT :
                     sys.exit()
 
-                if event.type == KEYDOWN :
-                    if event.key == K_ESCAPE :
-                        pygame.quit()
+                if eve.type == KEYDOWN :
+                    if eve.key == K_ESCAPE :
                         sys.exit()
 
-        pygame.display.update()
-
+    def start(self) :
+        if self.event == [] :
+            pygame.event.pump()
+        else :
+            for eve in self.event :
+                if eve.type == MOUSEBUTTONDOWN and eve.button == 1 :
+                    self.game_state = GAME_MODE['PLAY']
+                    print('now')
+            
+        
 """
 class Card_choice(pygame.sprite.Sprite) :
     animecycle = 12
